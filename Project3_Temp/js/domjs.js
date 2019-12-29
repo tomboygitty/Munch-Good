67220c644b78650014445eab5403e97012a64a9f
Stephen Fox <stephen@capablecreative.com>
2019-12-28 22:14:05 -0600

    Update with temporary html/css files - SLF

---
+++ Project3_Temp/js/domjs.js
@@ -0,0 +1,21 @@
+const refreshRate = 1000 / 60;
+const maxXPosition = 180;
+let rect = document.getElementById('radial');
+let speedX = 1 / 10;
+let positionX = 0;
+function step() {
+  positionX = positionX + speedX;
+  if (positionX > maxXPosition || positionX < 0) {
+    speedX = speedX * (-1);
+  }
+  rect.style.transform = 'rotate(' + positionX + 'deg)';
+  window.requestAnimationFrame(step);
+}
+window.requestAnimationFrame(step);
+
+
+let arcElement = document.getElementsByClassName('h1blackDescriptive');
+let arcText = document.getElementsByClassName('h1blackDescriptive')[0].innerHTML;
+
+console.log(arcText); 
+
