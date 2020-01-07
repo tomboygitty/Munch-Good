4f8b62d77ee961e20154f13ab1ed5f08310cd07a
Tom Elliott <xenomus@gmail.com>
2020-01-06 20:09:06 -0600

    changed PORT

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+const PORT = process.env.PORT || 3001;
+
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
+app.listen(PORT, () =>
   console.log('Express server is running on localhost:3001')
 );
