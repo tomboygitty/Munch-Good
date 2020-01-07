722dbd88088b8ce4d57179b272a1f5482ce256ca
Stephen Fox <stephen@capablecreative.com>
2020-01-06 21:25:28 -0600

    Routing issue server.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
