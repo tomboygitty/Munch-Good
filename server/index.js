1a2b7fa428636468a0b1ae210aa4c1eaf487c40b
Stephen Fox <stephen@capablecreative.com>
2020-01-08 18:44:53 -0600

    Update to hosting and other issues from Heroku deploy (server/index.js & package.json) - SLF

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('../build'));
 }
 
+app.get('*',(req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '../build') 
+  });
+});
+
 app.listen(PORT, () =>
-  console.log('Express server is running on localhost:3001')
+  console.log('Express server is running on localhost:3000'),
+  console.log(__dirname)
 );
