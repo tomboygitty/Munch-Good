439eaa3367aca3be36e14898bf6b9d71ede75f34
Tom Elliott <xenomus@gmail.com>
2020-01-17 20:35:08 -0600

    added README

---
+++ client/build/precache-manifest.56afcc3f3b34394aa869f2f95e5600e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2b02a8c446ccc162e272c54caceac7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "032ecb14759da93b66d0",
+    "url": "./static/css/main.a3f593b1.chunk.css"
+  },
+  {
+    "revision": "ecd0aa6549ddf3a9bf12",
+    "url": "./static/js/2.7432829e.chunk.js"
+  },
+  {
+    "revision": "20d13243d19698ac0a38f7729cbdf52a",
+    "url": "./static/js/2.7432829e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "032ecb14759da93b66d0",
+    "url": "./static/js/main.b4b154ac.chunk.js"
+  },
+  {
+    "revision": "3d9c77175a7e758a7364",
+    "url": "./static/js/runtime-main.20b5308b.js"
+  },
+  {
+    "revision": "bdc7b679038b75ec95d7a94758693c2e",
+    "url": "./static/media/allisonNhill.bdc7b679.png"
+  },
+  {
+    "revision": "edae681752ec1f8a01a21d9e19ac6c09",
+    "url": "./static/media/cloudBkgrd.edae6817.png"
+  },
+  {
+    "revision": "77afa37afcb531c20223928d909b8ac2",
+    "url": "./static/media/logo.77afa37a.png"
+  },
+  {
+    "revision": "b34cdbc6c33063b1f6300545fe8f43e5",
+    "url": "./static/media/radial.b34cdbc6.png"
+  },
+  {
+    "revision": "aa65c5b21275dbc09508133a23e3dcb3",
+    "url": "./static/media/rainbow.aa65c5b2.png"
+  },
+  {
+    "revision": "03b51558a3b40ca60f585f72efda7a3d",
+    "url": "./static/media/rifficfree-bold-webfont.03b51558.woff"
+  },
+  {
+    "revision": "54aff0c00c5233e45f0c698ca4b911e8",
+    "url": "./static/media/rifficfree-bold-webfont.54aff0c0.woff2"
+  },
+  {
+    "revision": "73be38047bd3a95edf9e90c5e4a4d4c7",
+    "url": "./static/media/rifficfree-bold_2-webfont.73be3804.woff2"
+  },
+  {
+    "revision": "e56476d415219dbb386340911d6f1fd9",
+    "url": "./static/media/rifficfree-bold_2-webfont.e56476d4.woff"
+  }
+]);
